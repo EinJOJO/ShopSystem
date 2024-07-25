471823a5887ce3248963d75c598d0cfb0918a87f
einjojo <einjojo@proton.me>
2024-07-25 11:26:04 +0200

    completed itemsetup

--- core/src/main/java/it/einjojo/shopsystem/setup/ShopItemSetup.java
+++ core/src/main/java/it/einjojo/shopsystem/setup/ShopItemSetup.java
@@ -82,7 +82,7 @@ public class ShopItemSetup extends AbstractSetup<ShopItem> {
                         currentNotNull(builder.getBuyPrice(), Object::toString));
                 setup.sendMessage("<green>4 <gray>- SellPrice ändern <dark_gray>(current: <gray><current>)",
                         currentNotNull(builder.getSellPrice(), Object::toString));
-                setup.sendMessage("<green>5 <gray>- Stock ändern <dark_gray>(current: <gray><current>)",
+                setup.sendMessage("<green>5 <gray>- Lagerbestand ändern <dark_gray>(current: <gray><current>)",
                         currentNotNull(builder.getStock(), Object::toString));
                 setup.sendMessage("<gray>Informationen:");
                 setup.sendMessage("<dark_gray> - <gray>Observer: <dark_gray>(current: <gray><current>)",
@@ -106,11 +106,30 @@ public class ShopItemSetup extends AbstractSetup<ShopItem> {
 
             @Override
             public void handleChat(ShopItemSetup setup, String message, AsyncChatEvent cancelledEvent) {
+                ISetupStage stage = null;
                 switch (message) {
                     case "1" -> {
-
+                        stage = TRADE_HANDLER_ITEM;
+                    }
+                    case "2" -> {
+                        stage = DISPLAY_ITEM;
+                    }
+                    case "3" -> {
+                        stage = BUY_PRICE;
+                    }
+                    case "4" -> {
+                        stage = SELL_PRICE;
+                    }
+                    case "5" -> {
+                        stage = STOCK;
+                    }
+                    default -> {
+                        setup.sendMessage("<red>Ungültige Eingabe!");
+                        return;
                     }
                 }
+                setup.confirmInput(message);
+                setup.setCurrentAndPrompt(stage);
             }
 
             @Override
@@ -118,8 +137,114 @@ public class ShopItemSetup extends AbstractSetup<ShopItem> {
                 cancelledEvent.setCancelled(false);
             }
         },
+        DISPLAY_ITEM {
+            @Override
+            public void handleChat(ShopItemSetup setup, String message, AsyncChatEvent cancelledEvent) {
+                cancelledEvent.setCancelled(false);
+            }
 
+            @Override
+            public void handleDrop(ShopItemSetup setup, ItemStack dropped, PlayerDropItemEvent cancelledEvent) {
+                if (dropped.getType().isAir()) {
+                    setup.sendMessage("<red>Item darf nicht Luft sein.");
+                    return;
+                }
+                setup.getBuilder().displayItem(dropped);
+                setup.confirmInput(dropped.getType().name());
+            }
+
+            @Override
+            public void prompt(ShopItemSetup setup) {
+                setup.sendMessage("<yellow>Droppe das Item, das im Shop zu sehen sein wird.");
+                setup.sendMessage("<gray>Das Item kann sich vom TradeHandler unterscheiden.");
+            }
+        },
+        BUY_PRICE {
+            @Override
+            public void handleChat(ShopItemSetup setup, String message, AsyncChatEvent cancelledEvent) {
+                try {
+                    int price = Integer.parseInt(message);
+                    if (price == -1) {
+                        setup.getBuilder().buyPrice(null);
+                        setup.confirmInput(message);
+                    } else {
+                        setup.getBuilder().buyPrice(price);
+                        setup.confirmInput("nicht verkaufbar.");
+                    }
+                    setup.setCurrentAndPrompt(Stage.ACTION_SELECT);
+                } catch (NumberFormatException ex) {
+                    setup.sendMessage("<red>Ungültige Eingabe! -1 & Positive ganze Zahlen");
+                }
+            }
+
+            @Override
+            public void handleDrop(ShopItemSetup setup, ItemStack dropped, PlayerDropItemEvent cancelledEvent) {
+                cancelledEvent.setCancelled(false);
+            }
+
+            @Override
+            public void prompt(ShopItemSetup setup) {
+                setup.sendMessage("<yellow>Gib den Kaufpreis an oder -1 für nicht verkaufbar.");
+            }
+        },
+        SELL_PRICE {
+            @Override
+            public void handleChat(ShopItemSetup setup, String message, AsyncChatEvent cancelledEvent) {
+                try {
+                    int price = Integer.parseInt(message);
+                    if (price == -1) {
+                        setup.getBuilder().sellPrice(null);
+                        setup.confirmInput(message);
+                    } else {
+                        setup.getBuilder().sellPrice(price);
+                        setup.confirmInput("nicht verkaufbar.");
+                    }
+                    setup.setCurrentAndPrompt(Stage.ACTION_SELECT);
+                } catch (NumberFormatException ex) {
+                    setup.sendMessage("<red>Ungültige Eingabe! -1 & Positive ganze Zahlen");
+                }
+            }
+
+            @Override
+            public void handleDrop(ShopItemSetup setup, ItemStack dropped, PlayerDropItemEvent cancelledEvent) {
+                cancelledEvent.setCancelled(false);
+            }
+
+            @Override
+            public void prompt(ShopItemSetup setup) {
+                setup.sendMessage("<yellow>Gib den Verkaufspreis an oder -1 für nicht verkaufbar.");
+            }
+        },
+        STOCK {
+            @Override
+            public void handleChat(ShopItemSetup setup, String message, AsyncChatEvent cancelledEvent) {
+                try {
+                    int stock = Integer.parseInt(message);
+                    if (stock == -1) {
+                        setup.getBuilder().stock(null);
+                        setup.confirmInput(message);
+                    } else {
+                        setup.getBuilder().stock(stock);
+                        setup.confirmInput("unendlich.");
+                    }
+                    setup.setCurrentAndPrompt(Stage.ACTION_SELECT);
+                } catch (NumberFormatException ex) {
+                    setup.sendMessage("<red>Ungültige Eingabe! -1 & Positive ganze Zahlen");
+                }
+            }
+
+            @Override
+            public void handleDrop(ShopItemSetup setup, ItemStack dropped, PlayerDropItemEvent cancelledEvent) {
+                cancelledEvent.setCancelled(false);
+            }
+
+            @Override
+            public void prompt(ShopItemSetup setup) {
+                setup.sendMessage("<yellow>Gib den Lagerbestand an oder -1 für unendlich.");
+            }
+        },
         TRADE_HANDLER_SELECTION { //TODO create more handler
+
             @Override
             public void handleChat(ShopItemSetup setup, String message, AsyncChatEvent cancelledEvent) {
 
